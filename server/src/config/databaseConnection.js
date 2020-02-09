8d8c8bfb8e35ad02eee9ae5d44969e54b1cd0738
Marta Pancaldi <marta.panc@gmail.com>
2020-02-09 16:22:44 +0000

    Add Mongo DB connection config

---
+++ server/src/config/databaseConnection.js
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient;
+const dbCredentials = require('./config').mongoDB;
+const util = require('util');
+
+let mongoDB;
+
+const connection = "mongodb+srv://%s:%s@cluster0-yx4fl.azure.mongodb.net/test?retryWrites=true&w=majority";
+
+const setupDB = callback => {
+    const uri = util.format(connection, dbCredentials.username, dbCredentials.password);
+
+    MongoClient.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        },
+        (error, client) => {
+            mongoDB = client.db('full-stack-server');
+
+            if (error) {
+                return callback(error);
+            }
+
+            return callback("Connected to Database")
+        });
+};
+
+const getDB = () => {
+    return mongoDB;
+};
+
+module.exports = {setupDB, getDB};
--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const graphqlHTTP = require('express-graphql');
+const { setupDB } = require('./config/databaseConnection');
 
 const schema = {};
 
 app.use(cors());
+
 app.use(
     '/graphql',
     graphqlHTTP({
@@ -14,6 +16,8 @@ app.use(
         pretty: true
     })
 );
+
+setupDB(v => console.log(v));
 
 app.listen(4000);
 console.log("Server listening on port 4000...");
